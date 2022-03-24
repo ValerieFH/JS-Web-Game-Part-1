25616b7f0c1b36f79cf2a999d4e2727f532fbe40
Valerie Haupt <valre@Valeries-MacBook-Pro-2.local>
2022-03-23 20:19:22 -0500

    completed bonus of part 1

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//function to create images and add item functionality
 function newImage(imgSrc,imgLeft,imgBottom,isItem = false) {
     let newImg = document.createElement('img')
     newImg.src = imgSrc
@@ -11,6 +12,32 @@ function newImage(imgSrc,imgLeft,imgBottom,isItem = false) {
     })}
 }
 
+//determine the number of 100x100 px tiles the window has
+let tileWide = Math.ceil(window.innerWidth/100)
+let tileTall = Math.ceil(window.innerHeight/100)
+
+//hardcodes the number of tiles for the grass
+let grassHeight = 5;
+
+//loops through each row of tiles in each column up to grassHeight
+for (let j = 0; j < grassHeight; j++) {
+    let pxBot = j*100;
+    for (let i = 0; i < tileWide; i++){
+        let pxLeft = i*100;
+        newImage('assets/grass.png',pxLeft + 'px',pxBot + 'px')
+    }
+}
+
+//fills remainer screen with sky
+for (let j = grassHeight; j < tileTall; j++) {
+    let pxBot = j*100;
+    for (let i = 0; i < tileWide; i++){
+        let pxLeft = i*100;
+        newImage('assets/sky.png',pxLeft + 'px',pxBot + 'px')
+    }
+}
+
+//adds images and items
 newImage('assets/green-character.gif','100px','100px')
 newImage('assets/pine-tree.png', '450px','200px')
 newImage('assets/tree.png','200px','300px')
@@ -21,4 +48,3 @@ newImage('assets/well.png','500px','425px')
 newImage('assets/sword.png','500px','405px',true)
 newImage('assets/shield.png','165px', '185px', true)
 newImage('assets/staff.png','600px','100px', true)
-
