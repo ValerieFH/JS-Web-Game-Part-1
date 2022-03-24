26b1d3dc6613c2f7984cf63954f653992dd0c31b
Valerie Haupt <valre@Valeries-MacBook-Pro-2.local>
2022-03-23 19:49:45 -0500

    completed part 1 base assignment

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(imgSrc,imgLeft,imgBottom,isItem = false) {
+    let newImg = document.createElement('img')
+    newImg.src = imgSrc
+    newImg.style.position = 'fixed'
+    newImg.style.left = imgLeft
+    newImg.style.bottom = imgBottom
+    document.body.append(newImg)
+    if (isItem){
+        newImg.addEventListener('dblclick', function(){
+                 newImg.remove()
+    })}
+}
+
+newImage('assets/green-character.gif','100px','100px')
+newImage('assets/pine-tree.png', '450px','200px')
+newImage('assets/tree.png','200px','300px')
+newImage('assets/pillar.png','350px','100px')
+newImage('assets/crate.png','150px','200px')
+newImage('assets/well.png','500px','425px')
+
+newImage('assets/sword.png','500px','405px',true)
+newImage('assets/shield.png','165px', '185px', true)
+newImage('assets/staff.png','600px','100px', true)
+
